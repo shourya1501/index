9d336615721b70e84c4d6421674831f1cf063234
shourya1501 <147493054+shourya1501@users.noreply.github.com>
2023-11-09 11:17:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,38 +15,38 @@ function App() {
         <tr id="id">
           <td>1</td>
           <td>Paragraph Word Counter</td>
-          <td><a href="https://github.com/abhimanyu90/wordcount">Repo Task-1</a></td>
-          <td><a href="https://wordcount-sandy.vercel.app/">Word Count</a></td>
+          <td><a href="https://github.com/shourya1501/wordcount">Repo Task-1</a></td>
+          <td><a href="https://wordcount-gold.vercel.app/">Word Count</a></td>
         </tr>
         <tr>
           <td>2</td>
           <td>Calculator</td>
-          <td><a href="https://github.com/abhimanyu90/calculator">Repo Task-2</a></td>
-          <td><a href="https://calculator-eight-lake.vercel.app/">Calculator</a></td>
+          <td><a href="https://github.com/shourya1501/calculator">Repo Task-2</a></td>
+          <td><a href="https://calculator-eta-two-13.vercel.app/">Calculator</a></td>
         </tr>
         <tr>
           <td>3</td>
           <td>Color Picker</td>
-          <td><a href="https://github.com/abhimanyu90/colorpicker">Repo Task-3</a></td>
-          <td><a href="https://colorpicker-puce.vercel.app/">Color Picker</a></td>
+          <td><a href="https://github.com/shourya1501/colorpicker">Repo Task-3</a></td>
+          <td><a href="https://colorpicker-three-ivory.vercel.app/">Color Picker</a></td>
         </tr>
         <tr>
           <td>4</td>
           <td>API Fetch</td>
-          <td><a href="https://github.com/abhimanyu90/database">Repo Task-4</a></td>
-          <td><a href="https://database-rho.vercel.app/">API Fetching</a></td>
+          <td><a href="https://github.com/shourya1501/database">Repo Task-4</a></td>
+          <td><a href="https://database-lac.vercel.app/">API Fetching</a></td>
         </tr>
         <tr>
           <td>5</td>
           <td>Age Calculator</td>
-          <td><a href="https://github.com/abhimanyu90/age">Repo Task-5</a></td>
-          <td><a href="https://age-woad.vercel.app/">Age Calculator</a></td>
+          <td><a href="https://github.com/shourya1501/age">Repo Task-5</a></td>
+          <td><a href="https://age-chi.vercel.app/">Age Calculator</a></td>
         </tr>
         <tr>
           <td>6</td>
           <td>PortFolio</td>
-          <td><a href="https://github.com/abhimanyu90/portfolio">Repo Task-6</a></td>
-          <td><a href="https://portfolio-mu-lake-78.vercel.app/">Portfolio</a></td>
+          <td><a href="https://github.com/shourya1501/portfolio">Repo Task-6</a></td>
+          <td><a href="https://portfolio-ten-psi-57.vercel.app/">Portfolio</a></td>
         </tr>
       </table>
       </center>
